3a3639fd1d0412d47c42bdfbe09f76bf83e50910
Michael Condouris <condour@gmail.com>
2023-08-11 16:29:48 -0400

    add python program

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print("===========\n")
+print("\nhello, world.\n")
+
+
+def simple_program(some_range):
+    for i in range(some_range):
+        print(i)
+
+
+simple_program(10)
+print("\ngoodbye.\n")
+print("===========\n")
